9d21d4e197ad9192cac658f8f114b555f88a6984
maru-ymic <151609378+maru-ymic@users.noreply.github.com>
2023-11-22 16:11:58 +0900

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,118 @@
+"use strict";
+
+jQuery(function ($) {
+  $('#mv').bgSwitcher({
+    images: ['../images/mv/slide1_pic_coverd.png', '../images/mv/slide2_pic_coverd.png', '../images/mv/slide3_pic_coverd.png'], // 切替背景画像を指定
+    interval: 4000, // 背景画像を切り替える間隔を指定 3000=3秒
+    loop: true, // 切り替えを繰り返すか指定 true=繰り返す false=繰り返さない
+    shuffle: false, // 背景画像の順番をシャッフルするか指定 true=する false=しない
+    effect: "fade", // エフェクトの種類をfade,blind,clip,slide,drop,hideから指定
+    duration: 1000, // エフェクトの時間を指定します。
+    easing: "swing" // エフェクトのイージングをlinear,swingから指定
+  });
+
+  // #で始まるa要素をクリックした場合に処理（"#"←ダブルクォーテンションで囲むのを忘れずに。忘れるとjQueryのバージョンによっては動かない。。）
+  $('a[href^="#"]').click(function () {
+    // 移動先を0px調整する。0を30にすると30px下にずらすことができる。
+    var adjust = -150;
+    // スクロールの速度（ミリ秒）
+    var speed = 700;
+    // アンカーの値取得 リンク先（href）を取得して、hrefという変数に代入
+    var href = $(this).attr("href");
+    // 移動先を取得 リンク先(href）のidがある要素を探して、targetに代入
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    // 移動先を調整 idの要素の位置をoffset()で取得して、positionに代入
+    var position = target.offset().top + adjust;
+    // スムーススクロール linear（等速） or swing（変速）
+    $('body,html').animate({ scrollTop: position }, speed, 'swing');
+    return false;
+  });
+});
+
+// メニュー展開時に背景を固定
+const backgroundFix = (bool) => {
+  const scrollingElement = () => {
+    const browser = window.navigator.userAgent.toLowerCase();
+    if ("scrollingElement" in document) return document.scrollingElement;
+    return document.documentElement;
+  };
+
+  const scrollY = bool
+    ? scrollingElement().scrollTop
+    : parseInt(document.body.style.top || "0");
+
+  const fixedStyles = {
+    height: "100vh",
+    position: "fixed",
+    top: `${scrollY * -1}px`,
+    left: "0",
+    width: "100vw"
+  };
+
+  Object.keys(fixedStyles).forEach((key) => {
+    document.body.style[key] = bool ? fixedStyles[key] : "";
+  });
+
+  if (!bool) {
+    window.scrollTo(0, scrollY * -1);
+  }
+};
+
+// 変数定義
+const CLASS = "-active";
+let flg = false;
+let accordionFlg = false;
+
+let humberger = document.getElementById("js-humberger");
+let focusTrap = document.getElementById("js-focus-trap");
+let menu = document.querySelector(".js-nav-area");
+let accordionTrigger = document.querySelectorAll(".js-sp-accordion-trigger");
+let accordion = document.querySelectorAll(".js-sp-accordion");
+
+// メニュー開閉制御
+humberger.addEventListener("click", (e) => { //ハンバーガーボタンが選択されたら
+  e.currentTarget.classList.toggle(CLASS);
+  menu.classList.toggle(CLASS);
+  if (flg) {// flgの状態で制御内容を切り替え
+    backgroundFix(false);
+    humberger.setAttribute("aria-expanded", "false");
+    humberger.focus();
+    flg = false;
+  } else {
+    backgroundFix(true);
+    humberger.setAttribute("aria-expanded", "true");
+    flg = true;
+  }
+});
+window.addEventListener("keydown", () => {　//escキー押下でメニューを閉じられるように
+  if (event.key === "Escape") {
+    humberger.classList.remove(CLASS);
+    menu.classList.remove(CLASS);
+
+    backgroundFix(false);
+    humberger.focus();
+    humberger.setAttribute("aria-expanded", "false");
+    flg = false;
+  }
+});
+
+// メニュー内アコーディオン制御
+accordionTrigger.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    e.currentTarget.classList.toggle(CLASS);
+    e.currentTarget.nextElementSibling.classList.toggle(CLASS);
+    if (accordionFlg) {
+      e.currentTarget.setAttribute("aria-expanded", "false");
+      accordionFlg = false;
+    } else {
+      e.currentTarget.setAttribute("aria-expanded", "true");
+      accordionFlg = true;
+    }
+  });
+
+});
+
+// フォーカストラップ制御
+focusTrap.addEventListener("focus", (e) => {
+  humberger.focus();
+});
